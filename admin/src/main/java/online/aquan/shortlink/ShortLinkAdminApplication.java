b1c58190bb735d97494701c1a3f6aa45d4865fa0
quan <3218684870@qq.com>
2024-01-22 21:19:50 +0800

    Initial commit

---
+++ admin/src/main/java/online/aquan/shortlink/ShortLinkAdminApplication.java
@@ -0,0 +1,12 @@
+package online.aquan.shortlink;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class);
+    }
+}
---
+++ gateway/src/main/java/online/aquan/shortlink/Main.java
@@ -0,0 +1,7 @@
+package online.aquan.shortlink;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
