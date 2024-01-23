db716868358b686df48285d30fddda207ade2fbd
quan <3218684870@qq.com>
2024-01-23 11:39:44 +0800

    feature: 自定义异常类

---
+++ admin/src/main/java/online/aquan/shortlink/admin/common/convention/exception/AbstractException.java
@@ -0,0 +1,31 @@
+package online.aquan.shortlink.admin.common.convention.exception;
+
+import com.google.protobuf.ServiceException;
+import lombok.Getter;
+import online.aquan.shortlink.admin.common.convention.errorcode.IErrorCode;
+import org.springframework.util.StringUtils;
+
+import java.util.Optional;
+
+/**
+ * 抽象项目中三类异常体系，客户端异常、服务端异常以及远程服务调用异常
+ *
+ * @see ClientException
+ * @see ServiceException
+ * @see RemoteException
+ */
+@Getter
+public abstract class AbstractException extends RuntimeException {
+
+    public final String errorCode;
+
+    public final String errorMessage;
+
+    public AbstractException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable);
+        this.errorCode = errorCode.code();
+        //这里首先检查message是否为空,如果为空那么就取出errorCode中的message
+        //orElse可以返回Optional里面的值,如果里面的值为null那么就返回默认值
+        this.errorMessage = Optional.ofNullable(StringUtils.hasLength(message) ? message : null).orElse(errorCode.message());
+    }
+}
