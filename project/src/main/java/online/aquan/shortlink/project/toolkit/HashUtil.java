8a27ff700d6d4e11a0f6d096505cbd6c59128e45
quan <3218684870@qq.com>
2024-01-24 16:58:34 +0800

    feature: 短链接生成算法

---
+++ project/src/main/java/online/aquan/shortlink/project/toolkit/HashUtil.java
@@ -0,0 +1,33 @@
+package online.aquan.shortlink.project.toolkit;
+
+import cn.hutool.core.lang.hash.MurmurHash;
+
+public class HashUtil {
+
+    private static final char[] CHARS = new char[]{
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
+    
+    private static final int SIZE = CHARS.length;
+
+    private static String convertDecToBase62(long num) {
+        StringBuilder sb = new StringBuilder();
+        while (num > 0) {
+            int i = (int) (num % SIZE);
+            sb.append(CHARS[i]);
+            num /= SIZE;
+        }
+        return sb.reverse().toString();
+    }
+
+    public static String hashToBase62(String str) {
+        //将给定的字符串进行32为的MurmurHash计算,将字符串映射成一个32位的整数
+        int i = MurmurHash.hash32(str);
+        //取正
+        long num = i < 0 ? Integer.MAX_VALUE - (long) i : i;
+        //将这个32位整数转换为62进制
+        return convertDecToBase62(num);
+    }
+}
