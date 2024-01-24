3c70270b47e85a9aedd3b8962173edb0871d776a
quan <3218684870@qq.com>
2024-01-24 11:35:45 +0800

    feature: 封装用户上下文

---
+++ admin/src/main/java/online/aquan/shortlink/admin/common/biz/user/UserInfoDto.java
@@ -0,0 +1,28 @@
+package online.aquan.shortlink.admin.common.biz.user;
+
+
+import com.alibaba.fastjson2.annotation.JSONField;
+import lombok.Data;
+
+/**
+ * 用户信息实体
+ */
+@Data
+public class UserInfoDto {
+
+    /**
+     * 用户 ID
+     */
+    @JSONField(name = "id")
+    private String userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+}
