7c8dd0da659e5bd8d9c3c6ab2bd4f1d72fd1e386
quan <3218684870@qq.com>
2024-02-12 19:29:33 +0800

    feature: 更新前端项目

--- console-vue/src/api/axios.js
+++ console-vue/src/api/axios.js
@@ -3,13 +3,14 @@ import { getToken, getUsername } from '@/core/auth.js'
 // import Router from '../router'
 import { ElMessage } from 'element-plus'
 import { isNotEmpty } from '@/utils/plugins.js'
-
+import { useRouter } from 'vue-router'
+const router = useRouter()
 // const baseURL = '/resourcesharing/organizational'
 const baseURL = '/api/short-link/admin/v1'
 // 创建实例
 const http = axios.create({
   // api 代理为服务器请求地址
-  baseURL: '/api' + baseURL,
+    baseURL: baseURL,
   timeout: 15000
 })
 // 请求拦截 -->在请求发送之前做一些事情
@@ -27,10 +28,6 @@ http.interceptors.request.use(
 // 响应拦截 -->在返回结果之前做一些事情
 http.interceptors.response.use(
   (res) => {
-    // 如果返回的code为 A000200代表登录过期或者没登录，将localStorage中的token清空
-    if (res.data.code === ' A000200') {
-      localStorage.removeItem('token')
-    }
     if (res.status == 0 || res.status == 200) {
       // 请求成功对响应数据做处理，此处返回的数据是axios.then(res)中接收的数据
       // code值为 0 或 200 时视为成功
@@ -41,6 +38,10 @@ http.interceptors.response.use(
   (err) => {
     // 在请求错误时要做的事儿
     // 此处返回的数据是axios.catch(err)中接收的数据
+    if (err.response.status === 401) {
+      localStorage.removeItem('token')
+      router.push('/login')
+    }
     return Promise.reject(err)
   }
 )
