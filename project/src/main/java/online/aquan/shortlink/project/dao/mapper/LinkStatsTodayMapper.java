409e90e086f2a547087bab7a216609ec984aa4b0
quan <3218684870@qq.com>
2024-02-13 19:28:37 +0800

    fix: 修复了今日统计数量的bug

--- project/src/main/java/online/aquan/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
+++ project/src/main/java/online/aquan/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -9,7 +9,7 @@ public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDo> {
 
     @Update(
             """
-                        insert into t_link_stats_today_0(gid, full_short_url, date, today_pv, today_uv, today_uip, create_time, update_time, del_flag)
+                        insert into t_link_stats_today(gid, full_short_url, date, today_pv, today_uv, today_uip, create_time, update_time, del_flag)
                         values (#{gid},#{fullShortUrl},#{date},#{todayPv},#{todayUv},#{todayUip},now(),now(),0)
                         on duplicate key update today_pv=today_pv+#{todayPv},today_uv = today_uv+#{todayUv},today_uip = today_uip+#{todayUip}
                     """
